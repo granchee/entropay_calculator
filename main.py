d7a47bf2ea8a383025be464e73e9e5beae63bfbf
Grant Hazelton <grant@thehazeltons.f2s.com>
2016-03-26 23:13:15 +0000

    Improved error-handling

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python
 
 # @todo Fee as arg?
-# @todo Proper validation of args?
 # @todo Updating .ini?
 
 import sys
 import ConfigParser
 
-def ValidateArg():
-    # Continue iff one proper arg supplied (assumed to be amount to be received).
-    if len(sys.argv) == 2:
-        is_valid = True
-    else:
-        print "Need numerical argument for amount to be received!"
-        is_valid = False
-    return is_valid
-
 def PrintUsage():
     print "----------------------------------------------------------------------"
     print "Usage: entropay_calculator.py amount-to-be-received"
 
+def GetAmountFromArg(arg):
+    try:
+        amount = float(arg)
+    except ValueError:
+        print "Need numerical argument for amount to be received!"
+        return 0.0
+    else:
+        return amount
+
 if __name__ == '__main__':
-    if False == ValidateArg():
+    if 2 != len(sys.argv):
         PrintUsage()
         sys.exit()
     else:
-        amount_to_be_received = float(sys.argv[1])
-        Config = ConfigParser.ConfigParser()
-        Config.read("entropay_calculator.ini")
-        percentage_fee = Config.getfloat("Config", "percentage_fee")
-        amount_to_be_sent = amount_to_be_received * 100.0 / (100.0 - percentage_fee)
-        print "Amount to be sent=%.2f (more exactly=%.3f)" % (amount_to_be_sent, amount_to_be_sent)
-        print "Using configured fee=%.2f%%" % (percentage_fee)
+        amount_to_be_received = GetAmountFromArg(sys.argv[1])
+        if 0.0 == amount_to_be_received:
+            PrintUsage()
+            sys.exit()
+        else:
+            Config = ConfigParser.ConfigParser()
+            try:
+                Config.read("entropay_calculator.ini")
+                percentage_fee = Config.getfloat("Config", "percentage_fee")
+                amount_to_be_sent = amount_to_be_received * 100.0 / (100.0 - percentage_fee)
+            except ConfigParser.Error:
+                print "Config problem?!"
+            except ZeroDivisionError:
+                print "Fee problem leading to divide-by-zero?!"
+            else:
+                print "Amount to be sent=%.2f (more exactly=%.3f)" % (amount_to_be_sent, amount_to_be_sent)
+                print "Using configured fee=%.2f%%" % (percentage_fee)
